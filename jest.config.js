604200fdb859a9633cd0e854b4855a5a5b7fc40d
AydMaxx <aydmaxx@gmail.com>
2025-07-04 09:56:39 +0400

    1

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
+  moduleNameMapping: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   testMatch: [
